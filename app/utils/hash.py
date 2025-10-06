f03bbaebdceda202c82fb6a19e144172778c6922
Dima <dimagudima61@gmail.com>
2025-10-06 14:13:13 +0300

    f3

--- app/utils/hash.py
+++ app/utils/hash.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-pwd_context = CryptContext(schemes=["bcrypt"], depracted="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(plain: str) -> str:
     return pwd_context.hash(plain)
